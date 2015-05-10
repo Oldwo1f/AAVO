8b9e3903523554bc76ae8760d5347bfbdc912e03
Alexis <alexismomcilovic@gmail.com>
2015-05-11 00:15:10 +0200

    initial commit

---
+++ config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  TOKEN_SECRET:'fe@!§ "ç_"_ç55dg48G5ze.ff#)',
+  MONGO_URI: 'localhost',
+  MAIN_EMAIL: 'toto@gmail.com',
+  MAIN_EMAIL_GOOGLE: 'alexismomcilovic@gmail.com',
+  MAIN_EMAIL_GOOGLE_PWD: 'Alexis09',
+  COMPANY_NAME:'TTT',
+  URL_HOME:'http://brandcolors.net/',
+  COMPANY_DOMAIN:'arbatou.fr',
+  PATH_TO_WEBSITE:'../nutrimarketing/',
+  PATH_TO_ADMIN:'../MADMIN/',
+  GOOGLE_ANALYTICS_ID:'',
+  DB_NAME:'test1',
+};
